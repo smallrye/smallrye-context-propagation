eae88aff058dbca9e9c9068bc61ade4182bcb1a5
Stephane Epardaud <stef@epardaud.fr>
2018-09-12 17:56:34 +0200

    Spec removed extra method so we're good for functional interfaces

--- tests/src/main/java/io/smallrye/concurrency/test/MyThreadContextProvider.java
+++ tests/src/main/java/io/smallrye/concurrency/test/MyThreadContextProvider.java
@@ -4,7 +4,6 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 
-import org.eclipse.microprofile.concurrent.spi.ThreadContextController;
 import org.eclipse.microprofile.concurrent.spi.ThreadContextProvider;
 import org.eclipse.microprofile.concurrent.spi.ThreadContextSnapshot;
 
@@ -16,20 +15,8 @@ public class MyThreadContextProvider implements ThreadContextProvider {
 		return () -> {
 			MyContext movedContext = MyContext.get();
 			MyContext.set(capturedContext);
-			return new ThreadContextController() {
-				private boolean ended = false;
-
-				@Override
-				public void endContext() throws IllegalStateException {
-					MyContext.set(movedContext);
-					ended = true;
-				}
-
-				@Override
-				public boolean isEnded() {
-					return ended;
-				}
-				
+			return () -> {
+				MyContext.set(movedContext);
 			};
 		};
 	}
@@ -39,23 +26,11 @@ public class MyThreadContextProvider implements ThreadContextProvider {
 		return () -> {
 			MyContext movedContext = MyContext.get();
 			MyContext.clear();
-			return new ThreadContextController() {
-				private boolean ended = false;
-
-				@Override
-				public void endContext() throws IllegalStateException {
-					if(movedContext == null)
-						MyContext.clear();
-					else
-						MyContext.set(movedContext);
-					ended = true;
-				}
-
-				@Override
-				public boolean isEnded() {
-					return ended;
-				}
-				
+			return () -> {
+				if(movedContext == null)
+					MyContext.clear();
+				else
+					MyContext.set(movedContext);
 			};
 		};
 	}
