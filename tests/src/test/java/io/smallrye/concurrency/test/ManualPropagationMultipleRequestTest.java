b76d7af0fe355a5c5bfbfa43ffb40d2226fb87ef
Stephane Epardaud <stef@epardaud.fr>
2019-01-03 16:45:07 +0100

    Test CDI context propagation

--- tests/src/test/java/io/smallrye/concurrency/test/ManualPropagationMultipleRequestTest.java
+++ tests/src/test/java/io/smallrye/concurrency/test/ManualPropagationMultipleRequestTest.java
@@ -1,13 +1,19 @@
 package io.smallrye.concurrency.test;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+import javax.enterprise.inject.spi.CDI;
+
 import org.eclipse.microprofile.concurrent.ManagedExecutor;
 import org.eclipse.microprofile.concurrent.spi.ConcurrencyProvider;
+import org.jboss.weld.context.bound.BoundRequestContext;
+import org.jboss.weld.environment.se.Weld;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -17,23 +23,51 @@ import io.smallrye.concurrency.impl.ThreadContextImpl;
 
 public class ManualPropagationMultipleRequestTest {
 	
-	private static ThreadContextImpl threadContext;
+	
+    public class Request {
+
+        public Map<String, Object> contextMap;
+        public BoundRequestContext cdiContext;
+
+        public Request(BoundRequestContext cdiContext, Map<String, Object> contextMap) {
+            this.cdiContext = cdiContext;
+            this.contextMap = contextMap;
+        }
+
+    }
+
+    private static ThreadContextImpl threadContext;
+    private static Weld weld;
 
 	@BeforeClass
 	public static void init() {
 		SmallRyeConcurrencyProvider.getManager();
 		threadContext = (ThreadContextImpl) ConcurrencyProvider.instance().getConcurrencyManager().newThreadContextBuilder().build();
+		
+        weld = new Weld();
+        weld.initialize();
 	}
 
-	public void newRequest(String reqId) {
+	public Request newRequest(String reqId) {
 		// seed
 		MyContext.init();
 		
 		MyContext.get().set(reqId);
+		
+        BoundRequestContext cdiContext = CDI.current().select(BoundRequestContext.class).get();
+        Map<String,Object> contextMap = new HashMap<String,Object>();
+        cdiContext.associate(contextMap);
+        cdiContext.activate();
+        
+        return new Request(cdiContext, contextMap);
 	}
 	
-	public void endOfRequest() {
+	public void endOfRequest(Request request) {
 		MyContext.clear();
+
+        request.cdiContext.invalidate();
+        request.cdiContext.deactivate();
+        request.cdiContext.dissociate(request.contextMap);
 	}
 
 	@Test
@@ -47,16 +81,16 @@ public class ManualPropagationMultipleRequestTest {
 	}
 
 	private void testRunnable(ExecutorService executor) throws Throwable {
-		newRequest("req 1");
+		Request req1 = newRequest("req 1");
 		Future<?> task1 = executor.submit(threadContext.contextualRunnable(() -> {
 			checkContextCaptured("req 1");
-			endOfRequest();
+			endOfRequest(req1);
 		}));
 		
-		newRequest("req 2");
+		Request req2 = newRequest("req 2");
 		Future<?> task2 = executor.submit(threadContext.contextualRunnable(() -> {
 			checkContextCaptured("req 2");
-			endOfRequest();
+			endOfRequest(req2);
 		}));
 
 		task1.get();
@@ -81,13 +115,13 @@ public class ManualPropagationMultipleRequestTest {
 
 		Throwable[] ret = new Throwable[2];
 
-		newRequest("req 1");
+		Request req1 = newRequest("req 1");
 		CompletableFuture<Void> cf1 = executor.newIncompleteFuture();
 		cf1.handleAsync((v, t) -> {
 			try {
 				ret[0] = t;
 				checkContextCaptured("req 1");
-				endOfRequest();
+				endOfRequest(req1);
 			}catch(Throwable t2) {
 				ret[0] = t2;
 			}
@@ -95,13 +129,13 @@ public class ManualPropagationMultipleRequestTest {
 			return null;
 		});
 		
-		newRequest("req 2");
+		Request req2 = newRequest("req 2");
 		CompletableFuture<Void> cf2 = executor.newIncompleteFuture();
 		cf2.handleAsync((v, t) -> {
 			try {
 				ret[1] = t;
 				checkContextCaptured("req 2");
-				endOfRequest();
+				endOfRequest(req2);
 			}catch(Throwable t2) {
 				ret[1] = t2;
 			}
