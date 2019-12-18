72f7ded3408cfd6aae05c9c2ca4446a49bef5b6c
Kabir Khan <kkhan@redhat.com>
2019-12-18 14:19:15 +0000

    Defer initialisation of the Config

--- tests/src/test/java/io/smallrye/context/test/BackPressureExceptionTest.java
+++ tests/src/test/java/io/smallrye/context/test/BackPressureExceptionTest.java
@@ -1,15 +1,15 @@
 package io.smallrye.context.test;
 
-import io.reactivex.observers.TestObserver;
-import io.smallrye.context.SmallRyeContextManagerProvider;
-import rx.observers.AssertableSubscriber;
+import static java.util.Arrays.asList;
+
+import java.util.concurrent.TimeUnit;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.util.concurrent.TimeUnit;
-
-import static java.util.Arrays.asList;
+import io.reactivex.observers.TestObserver;
+import io.smallrye.context.SmallRyeContextManagerProvider;
+import rx.observers.AssertableSubscriber;
 
 public class BackPressureExceptionTest {
 
@@ -19,30 +19,27 @@ public class BackPressureExceptionTest {
         SmallRyeContextManagerProvider.getManager();
     }
 
-
     @Test
     public void testBackPressureRxJava1() {
-        AssertableSubscriber<Integer> test =
-            rx.Observable.from(asList(1,2,3,4,5,6))
+        AssertableSubscriber<Integer> test = rx.Observable.from(asList(1, 2, 3, 4, 5, 6))
                 .concatMap(integer -> rx.Observable.just(integer).delay(100, TimeUnit.MILLISECONDS))
                 .test();
 
         test.awaitTerminalEvent();
 
         test.assertNoErrors();
-        test.assertReceivedOnNext(asList(1,2,3,4,5,6));
+        test.assertReceivedOnNext(asList(1, 2, 3, 4, 5, 6));
     }
 
     @Test
     public void testBackPressureRxJava2() {
-        TestObserver<Integer> test =
-            io.reactivex.Observable.just(1,2,3,4,5,6)
+        TestObserver<Integer> test = io.reactivex.Observable.just(1, 2, 3, 4, 5, 6)
                 .concatMap(integer -> io.reactivex.Observable.just(integer).delay(100, TimeUnit.MILLISECONDS))
                 .test();
 
         test.awaitTerminalEvent();
 
         test.assertNoErrors();
-        test.assertValues(1,2,3,4,5,6);
+        test.assertValues(1, 2, 3, 4, 5, 6);
     }
 }
