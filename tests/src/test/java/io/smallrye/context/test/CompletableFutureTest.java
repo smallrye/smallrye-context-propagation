0c3f734db1005d8c63978b0b67155a7f99b41476
Stephane Epardaud <stef@epardaud.fr>
2020-08-28 11:01:47 +0200

    Clean up TC in tests

--- tests/src/test/java/io/smallrye/context/test/CompletableFutureTest.java
+++ tests/src/test/java/io/smallrye/context/test/CompletableFutureTest.java
@@ -55,61 +55,65 @@ public class CompletableFutureTest {
         MyContext ctx = new MyContext();
         MyContext.set(ctx);
         Assert.assertEquals(ctx, MyContext.get());
-        // context in the current thread
-        managedExecutor.completedFuture(null)
-                .thenApply(v -> {
-                    Assert.assertEquals(ctx, MyContext.get());
-                    return v;
-                })
-                .get();
-        // context in the executor thread
-        managedExecutor.completedFuture(null)
-                .thenApplyAsync(v -> {
-                    Assert.assertEquals(ctx, MyContext.get());
-                    return v;
-                })
-                .get();
-        // still with context
-        Assert.assertEquals(ctx, MyContext.get());
+        try {
+            // context in the current thread
+            managedExecutor.completedFuture(null)
+                    .thenApply(v -> {
+                        Assert.assertEquals(ctx, MyContext.get());
+                        return v;
+                    })
+                    .get();
+            // context in the executor thread
+            managedExecutor.completedFuture(null)
+                    .thenApplyAsync(v -> {
+                        Assert.assertEquals(ctx, MyContext.get());
+                        return v;
+                    })
+                    .get();
+            // still with context
+            Assert.assertEquals(ctx, MyContext.get());
 
-        // now create a CF while we have a context
-        CompletableFuture<Void> cfCreatedWithContext = managedExecutor.completedFuture(null);
-        // remove the context
-        MyContext.clear();
+            // now create a CF while we have a context
+            CompletableFuture<Void> cfCreatedWithContext = managedExecutor.completedFuture(null);
+            // remove the context
+            MyContext.clear();
 
-        // check that we get no context, since we're capturing at lambda creation time
-        // nothing in the current thread
-        cfCreatedWithContext
-                .thenApply(v -> {
-                    Assert.assertNull(MyContext.get());
-                    return v;
-                })
-                .get();
-        // nothing in the executor thread
-        cfCreatedWithContext
-                .thenApplyAsync(v -> {
-                    Assert.assertNull(MyContext.get());
-                    return v;
-                })
-                .get();
+            // check that we get no context, since we're capturing at lambda creation time
+            // nothing in the current thread
+            cfCreatedWithContext
+                    .thenApply(v -> {
+                        Assert.assertNull(MyContext.get());
+                        return v;
+                    })
+                    .get();
+            // nothing in the executor thread
+            cfCreatedWithContext
+                    .thenApplyAsync(v -> {
+                        Assert.assertNull(MyContext.get());
+                        return v;
+                    })
+                    .get();
 
-        // now set it to another context and check we get the second context
-        MyContext ctx2 = new MyContext();
-        MyContext.set(ctx2);
-        // context in the current thread
-        cfCreatedWithContext
-                .thenApply(v -> {
-                    Assert.assertEquals(ctx2, MyContext.get());
-                    return v;
-                })
-                .get();
-        // context in the executor thread
-        cfCreatedWithContext
-                .thenApplyAsync(v -> {
-                    Assert.assertEquals(ctx2, MyContext.get());
-                    return v;
-                })
-                .get();
+            // now set it to another context and check we get the second context
+            MyContext ctx2 = new MyContext();
+            MyContext.set(ctx2);
+            // context in the current thread
+            cfCreatedWithContext
+                    .thenApply(v -> {
+                        Assert.assertEquals(ctx2, MyContext.get());
+                        return v;
+                    })
+                    .get();
+            // context in the executor thread
+            cfCreatedWithContext
+                    .thenApplyAsync(v -> {
+                        Assert.assertEquals(ctx2, MyContext.get());
+                        return v;
+                    })
+                    .get();
+        } finally {
+            MyContext.clear();
+        }
     }
 
     @Test
@@ -150,34 +154,38 @@ public class CompletableFutureTest {
 
         MyContext ctx = new MyContext();
         MyContext.set(ctx);
-        CompletableFuture<Object> asyncCompleteCF = managedExecutor.newIncompleteFuture().completeAsync(() -> {
-            Assert.assertEquals(ctx, MyContext.get());
-            return null;
-        });
-        Assert.assertTrue(asyncCompleteCF instanceof Contextualized);
-        Assert.assertNull(asyncCompleteCF.get());
-
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        asyncCompleteCF = managedExecutor.newIncompleteFuture().completeAsync(() -> {
-            Assert.assertEquals(ctx, MyContext.get());
-            return null;
-        }, executor);
-        Assert.assertTrue(asyncCompleteCF instanceof Contextualized);
-        Assert.assertNull(asyncCompleteCF.get());
-        executor.awaitTermination(2, TimeUnit.SECONDS);
-
-        CompletableFuture<Object> timeoutCF = managedExecutor.newIncompleteFuture().orTimeout(100, TimeUnit.MILLISECONDS);
-        Assert.assertTrue(timeoutCF instanceof Contextualized);
         try {
-            timeoutCF.get();
-            Assert.fail();
-        } catch (ExecutionException x) {
-            Assert.assertTrue(x.getCause() instanceof TimeoutException);
-        }
+            CompletableFuture<Object> asyncCompleteCF = managedExecutor.newIncompleteFuture().completeAsync(() -> {
+                Assert.assertEquals(ctx, MyContext.get());
+                return null;
+            });
+            Assert.assertTrue(asyncCompleteCF instanceof Contextualized);
+            Assert.assertNull(asyncCompleteCF.get());
 
-        timeoutCF = managedExecutor.newIncompleteFuture().completeOnTimeout(null, 100, TimeUnit.MILLISECONDS);
-        Assert.assertTrue(timeoutCF instanceof Contextualized);
-        Assert.assertNull(timeoutCF.get());
+            ExecutorService executor = Executors.newSingleThreadExecutor();
+            asyncCompleteCF = managedExecutor.newIncompleteFuture().completeAsync(() -> {
+                Assert.assertEquals(ctx, MyContext.get());
+                return null;
+            }, executor);
+            Assert.assertTrue(asyncCompleteCF instanceof Contextualized);
+            Assert.assertNull(asyncCompleteCF.get());
+            executor.awaitTermination(2, TimeUnit.SECONDS);
+
+            CompletableFuture<Object> timeoutCF = managedExecutor.newIncompleteFuture().orTimeout(100, TimeUnit.MILLISECONDS);
+            Assert.assertTrue(timeoutCF instanceof Contextualized);
+            try {
+                timeoutCF.get();
+                Assert.fail();
+            } catch (ExecutionException x) {
+                Assert.assertTrue(x.getCause() instanceof TimeoutException);
+            }
+
+            timeoutCF = managedExecutor.newIncompleteFuture().completeOnTimeout(null, 100, TimeUnit.MILLISECONDS);
+            Assert.assertTrue(timeoutCF instanceof Contextualized);
+            Assert.assertNull(timeoutCF.get());
+        } finally {
+            MyContext.clear();
+        }
     }
 
     @Test
@@ -189,66 +197,70 @@ public class CompletableFutureTest {
 
         MyContext ctx = new MyContext();
         MyContext.set(ctx);
-        CompletableFuture<String> cfNoContext = cf.thenApplyAsync(v -> {
-            // no context prop on new thread
-            Assert.assertNull(MyContext.get());
-            return v;
-        });
-        CompletableFuture<String> copyThisContext = copy.thenApplyAsync(v -> {
-            // context prop
-            Assert.assertEquals(ctx, MyContext.get());
-            return v;
-        });
-        // change the context to verify that we get the old context
-        MyContext ctx2 = new MyContext();
-        MyContext.set(ctx2);
+        try {
+            CompletableFuture<String> cfNoContext = cf.thenApplyAsync(v -> {
+                // no context prop on new thread
+                Assert.assertNull(MyContext.get());
+                return v;
+            });
+            CompletableFuture<String> copyThisContext = copy.thenApplyAsync(v -> {
+                // context prop
+                Assert.assertEquals(ctx, MyContext.get());
+                return v;
+            });
+            // change the context to verify that we get the old context
+            MyContext ctx2 = new MyContext();
+            MyContext.set(ctx2);
 
-        // check that completing cf completes both
-        cf.complete("OK");
-        Assert.assertEquals("OK", cfNoContext.get());
-        Assert.assertEquals("OK", copyThisContext.get());
+            // check that completing cf completes both
+            cf.complete("OK");
+            Assert.assertEquals("OK", cfNoContext.get());
+            Assert.assertEquals("OK", copyThisContext.get());
 
-        // make sure it has the same contexts
-        SmallRyeThreadContext threadContext = managedExecutor.getThreadContext();
-        Assert.assertNotNull(threadContext);
-        ThreadContextProviderPlan plan = threadContext.getPlan();
-        Assert.assertEquals(4, plan.clearedProviders.size());
-        Assert.assertTrue(plan.unchangedProviders.isEmpty());
-        Assert.assertEquals(1, plan.propagatedProviders.size());
+            // make sure it has the same contexts
+            SmallRyeThreadContext threadContext = managedExecutor.getThreadContext();
+            Assert.assertNotNull(threadContext);
+            ThreadContextProviderPlan plan = threadContext.getPlan();
+            Assert.assertEquals(4, plan.clearedProviders.size());
+            Assert.assertTrue(plan.unchangedProviders.isEmpty());
+            Assert.assertEquals(1, plan.propagatedProviders.size());
 
-        // now make sure ThreadContext can also copy those
-        CompletableFuture<String> cf2 = new CompletableFuture<>();
-        CompletableFuture<String> cf3 = new CompletableFuture<>();
-        CompletableFuture<String> cf2Copy = threadContext.withContextCapture(cf2);
-        // make it pass for a CS
-        CompletionStage<String> cf3Copy = threadContext.withContextCapture((CompletionStage<String>) cf3);
-        Assert.assertFalse(cf2.isDone());
-        Assert.assertFalse(cf2Copy.isDone());
+            // now make sure ThreadContext can also copy those
+            CompletableFuture<String> cf2 = new CompletableFuture<>();
+            CompletableFuture<String> cf3 = new CompletableFuture<>();
+            CompletableFuture<String> cf2Copy = threadContext.withContextCapture(cf2);
+            // make it pass for a CS
+            CompletionStage<String> cf3Copy = threadContext.withContextCapture((CompletionStage<String>) cf3);
+            Assert.assertFalse(cf2.isDone());
+            Assert.assertFalse(cf2Copy.isDone());
 
-        MyContext.set(ctx);
-        CompletableFuture<String> cf2NoContext = cf2.thenApplyAsync(v -> {
-            // no context prop on new thread
-            Assert.assertNull(MyContext.get());
-            return v;
-        });
-        CompletableFuture<String> cf2CopyThisContext = cf2Copy.thenApplyAsync(v -> {
-            // context prop
-            Assert.assertEquals(ctx, MyContext.get());
-            return v;
-        });
-        CompletionStage<String> cf3CopyThisContext = cf3Copy.thenApplyAsync(v -> {
-            // context prop
-            Assert.assertEquals(ctx, MyContext.get());
-            return v;
-        });
-        // change the context to verify that we get the old context
-        MyContext.set(ctx2);
+            MyContext.set(ctx);
+            CompletableFuture<String> cf2NoContext = cf2.thenApplyAsync(v -> {
+                // no context prop on new thread
+                Assert.assertNull(MyContext.get());
+                return v;
+            });
+            CompletableFuture<String> cf2CopyThisContext = cf2Copy.thenApplyAsync(v -> {
+                // context prop
+                Assert.assertEquals(ctx, MyContext.get());
+                return v;
+            });
+            CompletionStage<String> cf3CopyThisContext = cf3Copy.thenApplyAsync(v -> {
+                // context prop
+                Assert.assertEquals(ctx, MyContext.get());
+                return v;
+            });
+            // change the context to verify that we get the old context
+            MyContext.set(ctx2);
 
-        // check that completing cf completes both
-        cf2.complete("OK");
-        cf3.complete("OK");
-        Assert.assertEquals("OK", cf2NoContext.get());
-        Assert.assertEquals("OK", cf2CopyThisContext.get());
-        Assert.assertEquals("OK", cf3CopyThisContext.toCompletableFuture().get());
+            // check that completing cf completes both
+            cf2.complete("OK");
+            cf3.complete("OK");
+            Assert.assertEquals("OK", cf2NoContext.get());
+            Assert.assertEquals("OK", cf2CopyThisContext.get());
+            Assert.assertEquals("OK", cf3CopyThisContext.toCompletableFuture().get());
+        } finally {
+            MyContext.clear();
+        }
     }
 }
