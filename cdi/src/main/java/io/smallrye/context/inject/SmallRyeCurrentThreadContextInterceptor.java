a3a3006c93d43d895aab124d0e82c2d3380b7ed8
Stephane Epardaud <stef@epardaud.fr>
2020-09-02 14:17:36 +0200

    Use interceptor context to get to the annotation

--- cdi/src/main/java/io/smallrye/context/inject/SmallRyeCurrentThreadContextInterceptor.java
+++ cdi/src/main/java/io/smallrye/context/inject/SmallRyeCurrentThreadContextInterceptor.java
@@ -1,5 +1,7 @@
 package io.smallrye.context.inject;
 
+import java.util.Collection;
+
 import javax.annotation.Priority;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
@@ -16,15 +18,33 @@ public class SmallRyeCurrentThreadContextInterceptor {
 
     @AroundInvoke
     public Object manageCurrentContext(InvocationContext ctx) throws Exception {
-        CurrentThreadContext config = ctx.getMethod().getAnnotation(CurrentThreadContext.class);
-        SmallRyeThreadContext newTC = config.remove()
-                ? null
-                : SmallRyeThreadContext.builder()
-                        .cleared(config.cleared())
-                        .propagated(config.propagated())
-                        .unchanged(config.unchanged())
-                        .build();
-        try (CleanAutoCloseable ac = SmallRyeThreadContext.withThreadContext(newTC)) {
+        CurrentThreadContext config = null;
+        Object binding = ctx.getContextData().get("io.quarkus.arc.interceptorBindings");
+        if (binding == null)
+            binding = ctx.getContextData().get("org.jboss.weld.interceptor.bindings");
+        if (binding instanceof Collection) {
+            for (Object b : (Collection<?>) binding) {
+                if (b instanceof CurrentThreadContext) {
+                    config = (CurrentThreadContext) b;
+                    break;
+                }
+            }
+        }
+        if (config == null && ctx.getMethod() != null)
+            config = ctx.getMethod().getAnnotation(CurrentThreadContext.class);
+        if (config != null) {
+            SmallRyeThreadContext newTC = config.remove()
+                    ? null
+                    : SmallRyeThreadContext.builder()
+                            .cleared(config.cleared())
+                            .propagated(config.propagated())
+                            .unchanged(config.unchanged())
+                            .build();
+            try (CleanAutoCloseable ac = SmallRyeThreadContext.withThreadContext(newTC)) {
+                return ctx.proceed();
+            }
+        } else {
+            // could not find any config, that's an error, but we can continue
             return ctx.proceed();
         }
     }
